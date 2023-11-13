415eaac9c68fb6012911e4015e897cb0965278ee
DoubelT <thomasschuth@unimunich.org>
2023-11-13 12:59:54 +0100

    added inference loop

--- dpu_infer/dpu_infer.py
+++ dpu_infer/dpu_infer.py
@@ -365,7 +365,7 @@ def prep_data(arrayToChange):
     
 def postprocess(array):    
     post_array = []
-    for i in array.shape[0]:
+    for i in array.shape:
         post_array.append(sigmoid_rounded(array[i][0][0]))
     return array
 
