9a5d27a99f5438ed107d7d716afc361237cd830d
DoubelT <thschuth@gmail.com>
2024-01-21 23:19:53 +0100

    added fixed dpu inference

--- 4_8_1/dpu_infer/dpu_infer.py
+++ 4_8_1/dpu_infer/dpu_infer.py
@@ -290,28 +290,51 @@ data = [
 nested_input = np.array(data)
 
 
-def runBankNode(dpu_runner_tfBankNode):
-    
+def runBankNode(dpu_runner_tfBankNode, input, config):
+    config = config
+
     print("inside the run BankNodes..")
     inputTensors = dpu_runner_tfBankNode.get_input_tensors()  #  get the model input tensor
     outputTensors = dpu_runner_tfBankNode.get_output_tensors() # get the model ouput tensor
 
-    #Grab input Dimensions
     input_ndim = tuple(inputTensors[0].dims)
     output_ndim = tuple(outputTensors[0].dims)
 
-    #Print input and output Tensors
     print(inputTensors[0])
     print(outputTensors[0])
-   
-    #Preapare input data
+
+    runSize = 1
+    outputData = []
+
+    outputData.append([np.zeros((runSize, outputTensors[0].dims[1]), dtype=np.float32, order="C")])
+
+    shapeIn = (runSize,) + tuple([inputTensors[0].dims[i] for i in range(inputTensors[0].ndim)][1:])
+    print('Coded shapeIn: ', shapeIn)
+
+    #print('Input Tensor[0]: ', inputTensors[0])
+
+
+    inputData = []
+    inputData.append(input)
+
+
+    testOutput = []
+    testOutput.append(np.array([4],dtype=np.float32, order="C"))
+    testInput = []
+    
+    #Sample which is a fake so output should be 0
+    #testInput.append(np.array([1.6653357,1.0844043,-1.098024,-0.18494527], dtype=np.float32,order="C"))
+    
+    #Sample which is a orginal so output should be 1
+    #testInput.append(np.array([-0.6005844,-0.7122524,0.70745796,0.7955938], dtype=np.float32,order="C"))
+    
+    ##Feeding inputs to dpu
+    
     prepped_array = prep_data(nested_input)
     
-    #Create result array
-    result_array = []
+    testInput.append(prepped_array[0])
     
-    #Start Timer
-    time_start = time.time()
+    result_array = []
 
     print("Execute async")
     for i in range(prepped_array.shape[0]):
@@ -321,17 +344,8 @@ def runBankNode(dpu_runner_tfBankNode):
         dpu_runner_tfBankNode.wait(job_id)
         result_array.append(dataOutput)        
     
-    #Stop time for performance mesaurement
-    time_end = time.time()
-    timetotal = time_end - time_start
-    total_frames = 1
-    fps = float(total_frames / timetotal)
-    print(
-        "FPS=%.2f, total frames = %.2f , time=%.6f seconds"
-        % (fps, total_frames, timetotal)
-    )
-    #Finished executen starting postprocessing of data
     print("Execcution completed..")
+    print(len(result_array))
     print(postprocess(result_array))
    
 
@@ -391,9 +405,16 @@ def main(argv):
 
     # Preprocessing
 
+    validationset_features = np.loadtxt(r"../dataset/df_validationset_features",delimiter=',')
+    validationset_labels = np.loadtxt(r"../dataset/df_validationset_labels", delimiter=',')
+
+    print(validationset_features[0])
+
+    input = validationset_features[0]
+
 
     # Measure time
-    
+    time_start = time.time()
 
     """Assigns the runBankNode function with corresponding arguments"""
     print("runBankNode -- main function intialize")
@@ -402,7 +423,14 @@ def main(argv):
     del dpu_runners
     print("DPU runnerr deleted")
 
-    
+    time_end = time.time()
+    timetotal = time_end - time_start
+    total_frames = 1
+    fps = float(total_frames / timetotal)
+    print(
+        "FPS=%.2f, total frames = %.2f , time=%.6f seconds"
+        % (fps, total_frames, timetotal)
+    )
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
